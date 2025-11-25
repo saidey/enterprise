a15328259f88d7580db7b8f8c712f03fefbc38d0
Saifulla Ahmed <saidey@Saifullas-MacBook-Pro.local>
2025-11-25 09:48:24 +0500

    superadmin

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Contracts\Role as SpatieRoleContract;
 use Spatie\Permission\Traits\HasRoles;
+use RuntimeException;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use Auditable, HasApiTokens, HasFactory,HasRoles, Notifiable, UsesOrderedUuid;
+    use Auditable, HasApiTokens, HasFactory, Notifiable, UsesOrderedUuid;
+    use HasRoles {
+        assignRole as protected traitAssignRole;
+        syncRoles as protected traitSyncRoles;
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -72,5 +78,53 @@ class User extends Authenticatable
     public function defaultCompany()
     {
         return $this->companies()->wherePivot('is_default', true)->first();
+    }
+
+    public function assignRole(...$roles)
+    {
+        $this->ensureUniqueSuperadmin($roles);
+
+        return $this->traitAssignRole(...$roles);
+    }
+
+    public function syncRoles(...$roles)
+    {
+        $this->ensureUniqueSuperadmin($roles);
+
+        return $this->traitSyncRoles(...$roles);
+    }
+
+    private function ensureUniqueSuperadmin(array $rolesInput): void
+    {
+        $names = collect($rolesInput)
+            ->flatten()
+            ->map(function ($role) {
+                if ($role instanceof SpatieRoleContract) {
+                    return $role->name;
+                }
+                if (is_string($role)) {
+                    return $role;
+                }
+                if (is_int($role)) {
+                    return \App\Models\Role::find($role)?->name;
+                }
+
+                return null;
+            })
+            ->filter()
+            ->values()
+            ->all();
+
+        if (! in_array('superadmin', $names, true)) {
+            return;
+        }
+
+        $existingSuper = static::role('superadmin')
+            ->when($this->exists, fn ($q) => $q->where('id', '!=', $this->id))
+            ->first();
+
+        if ($existingSuper) {
+            throw new RuntimeException('Only one superadmin is allowed.');
+        }
     }
 }
